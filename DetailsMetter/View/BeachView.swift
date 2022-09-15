599cc8e8a88c9aec23822456245959a89edfeb25
canogg <88619478+canogg@users.noreply.github.com>
2022-09-15 10:02:22 -0300

    Sons e Haptics

--- DetailsMetter/View/BeachView.swift
+++ DetailsMetter/View/BeachView.swift
@@ -6,8 +6,12 @@
 //
 
 import UIKit
+import AVFoundation
 
 class BeachView: UIView {
+    
+    var player: AVAudioPlayer!
+    var i = 0
     
     lazy var backgroundImageView: UIImageView = {
         let backgroundImage = UIImage(named: "beachBackground")
@@ -16,11 +20,63 @@ class BeachView: UIView {
     
         return backgroundView
     }()
+    
+    lazy var createButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Botão", for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 24)
+        button.addTarget(self, action: #selector(buttonActions), for: .touchUpInside)
+        button.setTitleColor(.black, for: .normal)
+        button.clipsToBounds = true
+        button.layer.borderWidth = 1
+        button.layer.borderColor = UIColor.black.cgColor
+        button.layer.cornerRadius = 8
+       
+      return button
+        
+    }()
+    
+    @objc func buttonActions() {
+        playSound()
+        playHaptics()
+    }
 
+    func playSound(){
+        
+        
+       guard let url = Bundle.main.url(forResource: "success", withExtension: "mp3") else {
+           return
+       }
+        
+    
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+
+            guard let player = player else { return }
+
+            player.play()
+
+        } catch let error {
+            print(error.localizedDescription)
+
+        }
+    }
+    
+    func playHaptics() {
+        
+        HapticsManager.shared.vibrate(for: .success)
+        
+    }
+    
     override init(frame: CGRect) {
        super.init(frame: frame)
        
         self.addSubview(backgroundImageView)
+        self.addSubview(createButton)
         
         configureContraints()
     }
@@ -35,6 +91,9 @@ class BeachView: UIView {
             self.backgroundImageView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
             self.backgroundImageView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
             self.backgroundImageView.bottomAnchor.constraint(equalTo: self.bottomAnchor),
+            
+            self.createButton.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            self.createButton.centerYAnchor.constraint(equalTo: self.centerYAnchor),
         ])
     }
 }
